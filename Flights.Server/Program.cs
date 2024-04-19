63a64aa79325b1912c2ed17d5196da1524f1743e
AlSN1709 <152500597+AlSN1709@users.noreply.github.com>
2024-04-19 07:59:19 +0100

    day7

--- Flights.Server/Program.cs
+++ Flights.Server/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,15 +7,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(config=>config.AddServer(
-    new Microsoft.OpenApi.Models.OpenApiServer { Description="Dev",Url= "https://localhost:7133" }
-    ));
-
+builder.Services.AddSwaggerGen(config =>
+{
+    config.AddServer(
+        new OpenApiServer { Description = "Dev", Url = "https://localhost:7133" });
+    config.CustomOperationIds(e => $"{e.ActionDescriptor.RouteValues["action"] +
+        e.ActionDescriptor.RouteValues["controller"]}");
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseCors(builder => builder.WithOrigins("*"));
     app.UseSwagger();
     app.UseSwaggerUI();
 }
--- flights.client/src/app/search-flights/search-flights.component.ts
+++ flights.client/src/app/search-flights/search-flights.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FlightService } from '../api/services/flight.service';
+import { FlightRm } from '../api/models/flight-rm';
 @Component({
   selector: 'app-search-flights',
   templateUrl: './search-flights.component.html',
@@ -7,13 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchFlightsComponent implements OnInit {
 
-  constructor() { }
-  searchResult:any = [
-    'Tunisair',
-    'Nouvelair',
-    'AirFrance'
-  ]
+  constructor(private flightService: FlightService) { }
+  searchResult: FlightRm []=[]
   ngOnInit(): void {
+    this.search();
+  }
+  search() {
+    this.flightService.searchFlight(this.searchResult)
+      .subscribe(response => this.searchResult = response,
+        this.handleError)
+  }
+
+  private handleError(err: any) {
+    console.log("Response Error. Status: ", err.status)
+    console.log("Response Error. Status Text: ", err.statusText)
+    console.log(err)
   }
 
 }
+
